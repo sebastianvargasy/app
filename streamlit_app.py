9597f1d10c1d63495b2eaa619f9ba5b2ebf1e656
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 22:31:45 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import feedparser
 from datetime import datetime
-import pandas as pd
-from streamlit_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 
 st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
 
 # Creamos una lista con los feeds que queremos leer
 rss_feeds = [
-    "https://www.nytimes.com/sitemap.xml",
+    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed",
     "https://elpais.com/sitemap.xml",
     "https://www.bbc.com/news/sitemap.xml"
 ]
@@ -27,26 +25,17 @@ for rss_feed in rss_feeds:
         article['url'] = entry.link
         articles.append(article)
 
-# Convertimos la lista de diccionarios a un DataFrame de Pandas y ordenamos por fecha
-df = pd.DataFrame(articles)
-df['date'] = pd.to_datetime(df['date'], errors='coerce')
-df = df.sort_values('date', ascending=False).reset_index(drop=True)
+# Ordenamos la lista de artículos por fecha
+articles = sorted(articles, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)
 
 # Configuramos la tabla con masonry
-gb = GridOptionsBuilder.from_dataframe(df)
-gb.configure_column("feed", minWidth=120)
-gb.configure_column("title", minWidth=300)
-gb.configure_column("authors", minWidth=100)
-gb.configure_column("date", minWidth=120, valueFormatter="(date) 'YYYY-MM-DD HH:mm:ss'")
-gb.configure_column("summary", minWidth=300)
-go = gb.build()
-go.enable_masonry = True
-go.pagination = False
-go.rowModelType = 'clientSide'
-go.rowSelection = 'single'
-go.suppressRowClickSelection = True
-go.autoSizeColumns = True
-
-# Mostramos la tabla con masonry
-AgGrid(df, gridOptions=go, height=600, width='100%', update_mode=GridUpdateMode.SELECTION_CHANGED, data_return_mode=DataReturnMode.AS_FILTERED_AND_SORTED)
+columns = st.beta_columns(3)
+for article in articles:
+    col = columns.pop(0)
+    with col:
+        st.write(f"## {article['feed']}")
+        st.write(f"### [{article['title']}]({article['url']})")
+        st.write(f"Autor(es): {article['authors']}")
+        st.write(f"Fecha: {article['date']}")
+        st.write(article['summary'])
 
