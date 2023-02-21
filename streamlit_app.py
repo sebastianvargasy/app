44eb86abe6710a92c9eca446b00608424d8c9a48
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 22:35:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,12 @@
 import streamlit as st
 import feedparser
 from datetime import datetime
+import pandas as pd
 
 st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
 
 # Creamos una lista con los feeds que queremos leer
-rss_feeds = [
-    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed"
-]
+rss_feeds = [    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed",    "https://elpais.com/sitemap.xml",    "https://www.bbc.com/news/sitemap.xml"]
 
 # Leemos los feeds y guardamos los artículos en una lista de diccionarios
 articles = []
@@ -23,17 +22,10 @@ for rss_feed in rss_feeds:
         article['url'] = entry.link
         articles.append(article)
 
-# Ordenamos la lista de artículos por fecha
-articles = sorted(articles, key=lambda x: datetime.strptime(x['date'], '%Y-%m-%dT%H:%M:%SZ'), reverse=True)
+# Convertimos la lista de diccionarios a un DataFrame de Pandas y ordenamos por fecha
+df = pd.DataFrame(articles)
+df['date'] = pd.to_datetime(df['date'], errors='coerce')
+df = df.sort_values('date', ascending=False).reset_index(drop=True)
 
-# Configuramos la tabla con masonry
-columns = st.beta_columns(3)
-for article in articles:
-    col = columns.pop(0)
-    with col:
-        st.write(f"## {article['feed']}")
-        st.write(f"### [{article['title']}]({article['url']})")
-        st.write(f"Autor(es): {article['authors']}")
-        st.write(f"Fecha: {article['date']}")
-        st.write(article['summary'])
-
+# Mostramos la tabla de noticias en Pandas
+st.write(df)
