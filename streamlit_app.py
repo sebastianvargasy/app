7adb766925ef50727727ec00f23e3282ac13b8a0
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 23:01:59 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,9 @@
 import streamlit as st
 import feedparser
 import pandas as pd
-import html
 
 # Configuración de página
 st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
-
-# Función para crear enlaces clickeables
-def make_clickable(url):
-    if pd.isna(url):
-        return ""
-    url = html.escape(url)
-    return f'<a href="{url}" target="_blank">{url}</a>'
 
 # Creamos una lista con los feeds que queremos leer
 rss_feeds = [
@@ -44,15 +36,5 @@ if 'date' in df:
 # Ordenamos el DataFrame por fecha
 df = df.sort_values('date', ascending=False).reset_index(drop=True)
 
-# Agregamos una columna con enlaces clickeables
-df['url_html'] = df['url'].fillna('').astype(str).apply(make_clickable, axis=1)
-df = df[['feed', 'title', 'authors', 'date', 'summary', 'url_html']]
-
-# Encabezado de la página
-st.markdown("<h1 style='text-align: center; color: black;'>The Cyber House</h1>", unsafe_allow_html=True)
-
-# Tabla de noticias en Pandas
-st.write(df, unsafe_allow_html=True)
-
-# Pie de página
-st.markdown("<p style='text-align: center; color: black;'>By Sebastian Vargas</p>", unsafe_allow_html=True)
+# Mostramos la tabla de noticias en Pandas
+st.write(df)
