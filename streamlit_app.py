02eb541bf849d8365085cc094b4cff8bc15b3aaa
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 22:04:41 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,39 @@
 import streamlit as st
-import numpy as np
+import feedparser
+from newspaper import Article
 
-st.title("Simulation[tm]")
-st.write("Here is our super important simulation")
+st.set_page_config(page_title="Buscador de RSS", page_icon=":mag:", layout="wide")
 
-x = st.slider('Slope', min_value=0.01, max_value=0.10, step=0.01)
-y = st.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
+# Creamos una lista con los feeds que queremos leer
+rss_feeds = [
+    "https://www.nytimes.com/sitemap.xml",
+    "https://elpais.com/sitemap.xml",
+    "https://www.bbc.com/news/sitemap.xml"
+]
 
-st.write(f"x={x} y={y}")
+# Leemos los feeds y guardamos los artículos en una lista
+articles = []
+for rss_feed in rss_feeds:
+    feed = feedparser.parse(rss_feed)
+    for entry in feed.entries:
+        article = Article(entry.link)
+        article.download()
+        article.parse()
+        articles.append(article)
+
+# Creamos la barra de búsqueda
+search_query = st.text_input("Busca una noticia")
+
+# Filtramos los artículos según la búsqueda
+filtered_articles = []
+for article in articles:
+    if search_query.lower() in article.title.lower():
+        filtered_articles.append(article)
+
+# Mostramos los artículos filtrados
+for article in filtered_articles:
+    st.write(f"## {article.title}")
+    st.write(f"### {article.authors}")
+    st.write(f"Fecha: {article.publish_date}")
+    st.write(article.summary)
+    st.write(f"Leer más: [{article.source_url}]({article.url})")
