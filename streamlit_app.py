409f1b6b5788eccd67621ddda49b86be9e6b0daf
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-21 00:08:11 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write(" ")
 
 # Creamos una lista con los feeds que queremos leer
 rss_feeds = [
-    "https://www.cshub.com/rss/categories/attacks",
+    "https://www.cshub.com/rss/news",
     "https://feeds.feedburner.com/hispasec/zCAd"
 ]
 
