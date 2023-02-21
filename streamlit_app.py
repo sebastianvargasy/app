e1d31480063cb12e741bad10ba3f9726abbb29d6
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 22:44:16 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,9 @@ st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="
 
 # Creamos una lista con los feeds que queremos leer
 rss_feeds = [
-    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed"
+    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed",
+    "https://www.ccn-cert.cni.es/component/obrss/rss-ultimas-vulnerabilidades.feed",
+    "https://feeds.feedburner.com/hispasec/zCAd"
 ]
 
 # Leemos los feeds y guardamos los artículos en una lista de diccionarios
@@ -20,20 +22,27 @@ for rss_feed in rss_feeds:
         article['title'] = entry.title
         article['authors'] = entry.get('author', '')
         article['date'] = entry.get('published', '')
-        article['summary'] = entry.get('summary', '')
         article['url'] = entry.link
         articles.append(article)
 
 # Convertimos la lista de diccionarios a un DataFrame de Pandas
 df = pd.DataFrame(articles)
 
-# Verificamos si la columna "date" está presente en el DataFrame y, si es así, la convertimos a una columna de fecha
+# Convertimos la columna "date" a una columna de fecha
 if 'date' in df:
     df['date'] = pd.to_datetime(df['date'], errors='coerce')
 
 # Ordenamos el DataFrame por fecha
 df = df.sort_values('date', ascending=False).reset_index(drop=True)
 
+# Agregamos una columna con enlaces clickeables
+def make_clickable(url):
+    return f'<a href="{url}" target="_blank">{url}</a>'
+
+df['url_html'] = df['url'].apply(make_clickable, axis=1)
+df = df[['feed', 'title', 'authors', 'date', 'url_html']]
+
 # Mostramos la tabla de noticias en Pandas
-st.write(df)
+st.write(df, unsafe_allow_html=True)
+
 
