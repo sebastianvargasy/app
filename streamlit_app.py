bb9729d78d10c05018930cbb4b94680c515b83d1
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-21 00:26:04 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,52 @@
 
 import streamlit as st
 import feedparser
+import pandas as pd
 
 # Configuración de página
-st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
+st.set_page_config(page_title="El pasquin de Ciberseguridad", page_icon="📰", layout="wide")
 
 # Encabezado de la página
-st.markdown("<h1 style='text-align: center;'>The Ciber House</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>El pasquin de Ciberseguridad</h1>", unsafe_allow_html=True)
 st.write(" ")
 
 # Creamos una lista con los feeds que queremos leer
 rss_feeds = [
-    ("Noticias", "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed"),
-    ("Vulnerabilidades", "https://www.ccn-cert.cni.es/component/obrss/rss-ultimas-vulnerabilidades.feed"),
-    ("Hispasec", "https://feeds.feedburner.com/hispasec/zCAd")
+    "https://www.cisa.gov/uscert/ncas/current-activity.xml/",
+    "https://www.google.com/alerts/feeds/04729041965786430663/10669661265694116875",
+    "https://feeds.feedburner.com/NoticiasSeguridadInformatica",
+    "https://feeds.feedburner.com/hispasec/zCAd"
 ]
 
-dfs = {}
-
 # Leemos los feeds y guardamos los artículos en un diccionario de DataFrames de Pandas
-for title, rss_feed in rss_feeds:
+dfs = {}
+for rss_feed in rss_feeds:
     feed = feedparser.parse(rss_feed)
     articles = []
-    for entry in feed.entries:
+    for entry in feed.entries[:10]:  # Tomamos sólo las 10 noticias más recientes de cada feed
         article = {}
+        article['feed'] = feed.feed.title
         article['title'] = entry.title
         article['date'] = entry.get('published', '')
         article['summary'] = entry.get('summary', '')
         article['url'] = entry.link
         articles.append(article)
-
-    # Convertimos la lista de diccionarios a un DataFrame de Pandas
     df = pd.DataFrame(articles)
-
-    # Convertimos la columna "date" a una columna de fecha
     if 'date' in df:
         df['date'] = pd.to_datetime(df['date'], errors='coerce')
+        df = df.dropna(subset=['date'])  # Eliminamos las filas con fechas nulas
+        df = df.sort_values('date', ascending=False).reset_index(drop=True)
+        dfs[feed.feed.title] = df
 
-    # Ordenamos el DataFrame por fecha
-    df = df.sort_values('date', ascending=False).reset_index(drop=True)
+# Mostramos las noticias en pestañas separadas para cada feed
+if dfs:
+    feed_names = list(dfs.keys())
+    feed_index = st.sidebar.selectbox("Selecciona un feed:", feed_names)
+    st.write(f"### Noticias de {feed_index}")
+    st.write(dfs[feed_index])
+else:
+    st.write("No se pudieron leer los feeds de noticias.")    
 
-    dfs[title] = df
-
-# Creamos una pestaña para cada feed
-for title, df in dfs.items():
-    st.write(f"# {title}")
-    for _, row in df.iterrows():
-        st.write(f"<h2>{row.title}</h2>", unsafe_allow_html=True)
-        st.write(f"<p><em>{row.date}</em></p>", unsafe_allow_html=True)
-        st.write(f"<p>{row.summary}</p>", unsafe_allow_html=True)
-        st.write(f"<a href='{row.url}'>Leer más</a>", unsafe_allow_html=True)
-        st.write("<hr>", unsafe_allow_html=True)
-
+# Pie de página
+st.write(" ")
+st.markdown("<p style='text-align: center;'>Hecho con ❤️ por Sebastian Vargas</p>", unsafe_allow_html=True)
