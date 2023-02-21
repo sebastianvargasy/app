9b8520ca8959a1abf75804cbf4558e3dc601a92f
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 22:17:13 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import feedparser
-from newspaper import Article
 
 st.set_page_config(page_title="Buscador de RSS", page_icon=":mag:", layout="wide")
 
@@ -16,9 +15,12 @@ articles = []
 for rss_feed in rss_feeds:
     feed = feedparser.parse(rss_feed)
     for entry in feed.entries:
-        article = Article(entry.link)
-        article.download()
-        article.parse()
+        article = {}
+        article['title'] = entry.title
+        article['authors'] = entry.get('author', '')
+        article['date'] = entry.get('published', '')
+        article['summary'] = entry.get('summary', '')
+        article['url'] = entry.link
         articles.append(article)
 
 # Creamos la barra de búsqueda
@@ -27,13 +29,13 @@ search_query = st.text_input("Busca una noticia")
 # Filtramos los artículos según la búsqueda
 filtered_articles = []
 for article in articles:
-    if search_query.lower() in article.title.lower():
+    if search_query.lower() in article['title'].lower():
         filtered_articles.append(article)
 
 # Mostramos los artículos filtrados
 for article in filtered_articles:
-    st.write(f"## {article.title}")
-    st.write(f"### {article.authors}")
-    st.write(f"Fecha: {article.publish_date}")
-    st.write(article.summary)
-    st.write(f"Leer más: [{article.source_url}]({article.url})")
+    st.write(f"## {article['title']}")
+    st.write(f"### {article['authors']}")
+    st.write(f"Fecha: {article['date']}")
+    st.write(article['summary'])
+    st.write(f"Leer más: [{article['url']}]({article['url']})")
