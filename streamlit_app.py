20b3ad6fd14109df5ac06a2b9c708176a936458a
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 23:27:58 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,56 +1,32 @@
-
 import streamlit as st
-import feedparser
-import pandas as pd
+import requests
+import schedule
+import time
 
-st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
-st.title("The Ciber House")
+st.set_page_config(page_title="Web Monitor", page_icon=":computer:")
 
-# Leer feeds RSS
-rss_feeds = [    "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed",    "https://www.ccn-cert.cni.es/component/obrss/rss-ultimas-vulnerabilidades.feed",    "https://feeds.feedburner.com/hispasec/zCAd"]
+site_url = st.text_input("Ingrese la URL del sitio web a monitorear")
 
-articles = []
-for rss_feed in rss_feeds:
-    feed = feedparser.parse(rss_feed)
-    for entry in feed.entries[:5]:  # Leer las 5 entradas más recientes
-        article = {}
-        article['feed'] = feed.feed.title
-        article['title'] = entry.title
-        article['date'] = entry.get('published', '')
-        article['summary'] = entry.get('summary', '')
-        article['url'] = entry.link
-        articles.append(article)
+status_text = st.empty()
 
-df = pd.DataFrame(articles)
-df['date'] = pd.to_datetime(df['date'], errors='coerce')
-df = df.sort_values('date', ascending=False).reset_index(drop=True)
+history = []
 
-# Mostrar la tabla de noticias
-st.write("Noticias RSS más recientes")
-st.dataframe(df.style.hide_columns(['authors'])) 
+def check_site():
+    try:
+        response = requests.get(site_url)
+        status_code = response.status_code
+        if status_code == 200:
+            status_text.success("El sitio está en línea.")
+        else:
+            status_text.warning("El sitio está en línea, pero ha devuelto un código de estado distinto de 200.")
+    except requests.exceptions.ConnectionError:
+        status_text.error("El sitio no está disponible.")
+        status_code = None
+    
+    history.append({"timestamp": time.time(), "status": status_code})
 
-# Leer feed del Zero Day Initiative
-zdi_feed = "https://www.zerodayinitiative.com/rss/zdi-all.xml"
+schedule.every(5).seconds.do(check_site)
 
-zdi_articles = []
-zdi_feed = feedparser.parse(zdi_feed)
-for entry in zdi_feed.entries[:10]:  # Leer las 10 entradas más recientes
-    zdi_article = {}
-    zdi_article['title'] = entry.title
-    zdi_article['date'] = entry.get('published', '')
-    zdi_article['summary'] = entry.get('summary', '')
-    zdi_article['url'] = entry.link
-    zdi_articles.append(zdi_article)
-
-zdi_df = pd.DataFrame(zdi_articles)
-zdi_df['date'] = pd.to_datetime(zdi_df['date'], errors='coerce')
-zdi_df = zdi_df.sort_values('date', ascending=False).reset_index(drop=True)
-
-# Mostrar la tabla de noticias del Zero Day Initiative con paginación
-st.write("Noticias Zero Day Initiative más recientes")
-page_size = 10
-page = st.sidebar.slider("Página", 1, int(len(zdi_df) / page_size) + 1)
-start = (page - 1) * page_size
-end = start + page_size
-st.dataframe(zdi_df.iloc[start:end].style.hide_index())
-st.sidebar.write("By Sebastian Vargas") 
+while True:
+    schedule.run_pending()
+    time.sleep(1)
