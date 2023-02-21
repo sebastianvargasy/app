9e8365ff1cb31dc5db62eae085bdd8272731be1b
sebastianvargasy <64619819+sebastianvargasy@users.noreply.github.com>
2023-02-20 23:58:19 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,50 +6,36 @@ import pandas as pd
 st.set_page_config(page_title="Noticias RSS en HTML", page_icon="📰", layout="wide")
 
 # Encabezado de la página
-st.header("The Ciber House")
+st.markdown("<h1 style='text-align: center;'>The Ciber House</h1>", unsafe_allow_html=True)
 st.write(" ")
 
-# Creamos una lista con los feeds que queremos leer
-rss_feeds = [
-    "https://www.cshub.com/rss/categories/attacks",
-    "https://www.cshub.com/rss/news",
-    "https://feeds.feedburner.com/hispasec/zCAd"
-]
+# Creamos una lista de tuplas con los RSS que queremos leer y su nombre para la pestaña correspondiente
+rss_feeds = [    ("CCN-CERT", "https://www.ccn-cert.cni.es/component/obrss/rss-noticias.feed"),    ("Vulnerabilidades", "https://www.ccn-cert.cni.es/component/obrss/rss-ultimas-vulnerabilidades.feed"),    ("Hispasec", "https://feeds.feedburner.com/hispasec/zCAd")]
 
-# Leemos los feeds y guardamos los artículos en una lista de diccionarios
-articles = []
-for rss_feed in rss_feeds:
+# Leemos los feeds y guardamos los artículos en un diccionario donde la clave es el nombre del feed
+dfs = {}
+for rss_name, rss_feed in rss_feeds:
     feed = feedparser.parse(rss_feed)
-    for entry in feed.entries[:10]:  # Tomamos sólo las 10 noticias más recientes
+    articles = []
+    for entry in feed.entries[:10]:  # Tomamos sólo las 10 noticias más recientes de cada feed
         article = {}
-        article['feed'] = feed.feed.title
         article['title'] = entry.title
         article['date'] = entry.get('published', '')
         article['summary'] = entry.get('summary', '')
         article['url'] = entry.link
         articles.append(article)
+    df = pd.DataFrame(articles)
+    if 'date' in df:
+        df['date'] = pd.to_datetime(df['date'], errors='coerce')
+    df = df.sort_values('date', ascending=False).reset_index(drop=True)
+    dfs[rss_name] = df
 
-# Convertimos la lista de diccionarios a un DataFrame de Pandas
-df = pd.DataFrame(articles)
+# Creamos las pestañas y mostramos las tablas correspondientes
+tabs = st.tabs(list(dfs.keys()))
+for i, rss_name in enumerate(dfs.keys()):
+    with tabs[i]:
+        st.write(rss_name)
+        st.write(dfs[rss_name])
 
-# Convertimos la columna "date" a una columna de fecha
-if 'date' in df:
-    df['date'] = pd.to_datetime(df['date'], errors='coerce')
-
-# Ordenamos el DataFrame por fecha
-df = df.sort_values('date', ascending=False).reset_index(drop=True)
-
-# Limitamos a las últimas 30 noticias
-df = df[:30]
-
-# Agrupamos los artículos por feed
-dfs = {}
-for feed in df.feed.unique():
-    dfs[feed] = df[df.feed == feed]
-
-# Creamos una pestaña por feed
-for feed, df in dfs.items():
-    st.write(f"## {feed}")
-    st.write(df.to_html(escape=False), unsafe_allow_html=True)
 
 
